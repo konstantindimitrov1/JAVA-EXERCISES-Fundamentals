ac7b152d429bbf85eb0aa571b914ca9cbe71265f
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-06-11 14:06:14 +0300

    Added new exercise.

--- RegularExpressions/MatchPhoneNumber.java
+++ RegularExpressions/MatchPhoneNumber.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "([+]359[ ]2[ ]\\d{3}[ ]\\d{4}\\b)|([+]359[-]2[-]\\d{3}[-]\\d{4}\\b)";
+
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> phones = new ArrayList<>();
+
+        while (matcher.find()) {
+            phones.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", phones));
+    }
 }
