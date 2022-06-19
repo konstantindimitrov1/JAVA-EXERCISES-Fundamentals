34a30b5e2a6e24b5a7c49a6e3bd6cb231c7563d0
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-06-19 22:34:31 +0300

    Added new exercise.

--- RegularExpressions/SoftUniBarIncome.java
+++ RegularExpressions/SoftUniBarIncome.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SoftUniBarIncome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double totalIncome = 0;
+
+        String input = scanner.nextLine();
+
+        String regex = "%(?<name>[A-Z][a-z]+)%[^$.|%]*?<(?<product>\\w+)>[^$.|%]*?[|](?<quantity>\\d+)[|][^$.|%]*?(?<price>\\d+[.]?\\d+)[$]";
+
+        while (!input.equals("end of shift")) {
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                String name = matcher.group("name");
+                String product = matcher.group("product");
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+                double price = Double.parseDouble(matcher.group("price"));
+                double sumForCurrentPurchase = price * quantity;
+                totalIncome += sumForCurrentPurchase;
+
+                System.out.printf("%s: %s - %.2f\n", name, product, sumForCurrentPurchase);
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+        System.out.printf("Total income: %.2f", totalIncome);
+
+    }
 }
