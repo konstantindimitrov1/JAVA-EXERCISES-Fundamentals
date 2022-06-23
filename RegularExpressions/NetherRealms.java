e7313d299c8dd3c3be889918f048572aecce8bff
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-06-23 21:14:59 +0300

    Added new exercise.

--- RegularExpressions/NetherRealms.java
+++ RegularExpressions/NetherRealms.java
@@ -1,7 +1,8 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class NetherRealms {
     public static void main(String[] args) {
