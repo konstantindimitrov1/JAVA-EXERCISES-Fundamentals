07fed9ed8e1fc5dd46827da4a64c3ca1e7ad6ae9
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-06-28 20:43:31 +0300

    Added new exercise.

--- RegularExpressions/ExtractEmails.java
+++ RegularExpressions/ExtractEmails.java
@@ -1,3 +1,19 @@
-public class ExtractEmails {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class ExtractEmails {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("(^|(?<=\\s))(([a-zA-Z0-9]+)([\\-_]?)([A-Za-z0-9]+)(@)([a-zA-Z]+([\\-][A-Za-z]+)+))(\\b|(?=\\s))");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+
+    }
 }
